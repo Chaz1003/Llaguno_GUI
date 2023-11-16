6c6a54ad755b8418c313794943c36d7115385882
LAB12_USER ON WS16 <L12Y16W16@citulab.net>
2023-11-16 17:19:47 +0800

    Leap Year

--- src/LeapYear/LeapYear.java
+++ src/LeapYear/LeapYear.java
@@ -1,2 +1,35 @@
-package LeapYear;public class LeapYear {
+package LeapYear;
+
+import javax.swing.*;
+
+public class LeapYear extends JFrame{
+    private JPanel jpanel;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+    private JLabel InputYear;
+    public LeapYear(){
+        btnCheckYear.addActionListener(e->{
+        int year = Integer.parseInt(tfYear.getText());
+            if(year%4==0){
+                if(year%400!=0){
+                    JOptionPane.showMessageDialog(this,"Not a leap year");
+                    return;
+                }
+                JOptionPane.showMessageDialog(this,"Leap year");
+                return;
+            }
+            JOptionPane.showMessageDialog(this,"Not a leap year");
+            return;
+        });
+    }
+
+    public static void main(String[] args){
+        LeapYear app = new LeapYear();
+        app.setContentPane(app.jpanel);
+        app.setSize(500,300);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("Leap Year Checker");
+    }
 }
+
